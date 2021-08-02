393e751e34fa3a02021fd011f5e2a2be90a43a35
ketkimude137 <88311039+ketkimude137@users.noreply.github.com>
2021-08-02 14:31:16 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,17 @@
+#addition
+def add(x,y):
+    return x+y #on master branch
+#subtract
+def subtract(x,y):
+    return x-y   #on master branch
+
+#multiply
+def multiply(x,y):
+    return x*y #on bug456
+#divide
+
+def divide(x,y):
+    if y==0:
+    return Divide_by_0_error
+    else:
+        return x/y
